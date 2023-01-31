56e675af5c598a883ffc056e4746a1d5658298f9
Neetu Seharawat <neetumorbca@gmail.com>
2023-01-31 18:37:16 +0530

    College Added

--- src/Components/UI/ErrorModal.js
+++ src/Components/UI/ErrorModal.js
@@ -2,12 +2,17 @@ import React from 'react';
 import Card from './Card';
 import Button from './Button';
 import classes from './ErrorModal.module.css';
+import ReactDOM from 'react-dom';
 
-const ErrorModal = (props) => {
-  return (
-    <React.Fragment>
-      <div className={classes.backdrop} onClick={props.onConfirm} />
-      <Card className={classes.modal}>
+const Backdrop = props =>{
+  return(
+    <div className={classes.backdrop} onClick={props.onConfirm} />
+  );
+};
+
+const ModelOverlay =props =>{
+return(
+  <Card className={classes.modal}>
         <header className={classes.header}>
           <h2>{props.title}</h2>
         </header>
@@ -18,6 +23,23 @@ const ErrorModal = (props) => {
           <Button onClick={props.onConfirm}>Okay</Button>
         </footer>
       </Card>
+);
+};
+const ErrorModal = (props) => {
+  return (
+    <React.Fragment>
+    {
+     ReactDOM.createPortal(
+     <Backdrop onConfirm= {props.onConfirm} />,
+     document.getElementById('backdrop-root')
+     )}  
+     {ReactDOM.createPortal(
+     <ModelOverlay 
+     title ={props.title}
+     message={props.message}
+     onConfirm ={props.onConfirm}
+     />
+     )}
     </React.Fragment>
   );
 };
--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 import Card from '../UI/Card';
 import Button from '../UI/Button';
@@ -6,16 +6,24 @@ import ErrorModal from '../UI/ErrorModal';
 import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState('');
-  const [enteredAge, setEnteredAge] = useState('');
+ const nameInputRef = useRef();
+ const ageInputRef = useRef();
+ const collegeInputRef = useRef();
+
+  
   const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+    const enteredName =nameInputRef.current.value;
+    const enteredAge =ageInputRef.current.value;
+    const enteredCollege =collegeInputRef.current.value;
+
+
+    if (enteredName.trim().length === 0 || enteredAge.trim().length === 0 ||enteredCollege.trim().length === 0) {
       setError({
         title: 'Invalid input',
-        message: 'Please enter a valid name and age (non-empty values).',
+        message: 'Please enter a valid name,age and college (non-empty values).',
       });
       return;
     }
@@ -26,17 +34,11 @@ const AddUser = (props) => {
       });
       return;
     }
-    props.onAddUser(enteredUsername, enteredAge);
-    setEnteredUsername('');
-    setEnteredAge('');
-  };
+    props.onAddUser(enteredName, enteredAge, enteredCollege);
+    nameInputRef.current.value='';
+    ageInputRef.current.value ='';
+    collegeInputRef.current.value='';
 
-  const usernameChangeHandler = (event) => {
-    setEnteredUsername(event.target.value);
-  };
-
-  const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value);
   };
 
   const errorHandler = () => {
@@ -58,15 +60,19 @@ const AddUser = (props) => {
           <input
             id="username"
             type="text"
-            value={enteredUsername}
-            onChange={usernameChangeHandler}
+            ref ={nameInputRef}
           />
           <label htmlFor="age">Age (Years)</label>
           <input
             id="age"
             type="number"
-            value={enteredAge}
-            onChange={ageChangeHandler}
+            ref ={ageInputRef}
+          />
+           <label htmlFor="userCollege">userCollege</label>
+          <input
+            id="usercollege"
+            type="text"
+            ref ={collegeInputRef}
           />
           <Button type="submit">Add User</Button>
         </form>
